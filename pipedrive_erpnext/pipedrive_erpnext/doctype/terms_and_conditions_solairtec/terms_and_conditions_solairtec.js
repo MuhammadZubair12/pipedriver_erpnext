33ff1207eb3f2393e503833d59d8cb27b6086772
developers@techstation.com.eg <developers@techstation.com.eg>
2023-08-01 11:33:47 +0000

    Latest

---
+++ pipedrive_erpnext/pipedrive_erpnext/doctype/terms_and_conditions_solairtec/terms_and_conditions_solairtec.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, zubair and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Terms and Conditions Solairtec', {
+	// refresh: function(frm) {
+
+	// }
+});
